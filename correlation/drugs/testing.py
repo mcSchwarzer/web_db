3f9311de782664f5e126c4d790bf1dda23d4c07c
mhuber97 <31443865+mhuber97@users.noreply.github.com>
2018-04-13 13:30:09 +0200

    Add files via upload

---
+++ correlation/drugs/testing.py
@@ -0,0 +1,82 @@
+import numpy as numpy
+import json
+from pprint import pprint
+import datetime
+import time
+
+fulldates = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
+     2008, 2009, 2010, 2011, 2012, 2013, 2014]
+
+data = json.load(open('drug_poisining_mortality_usa.json'))
+vorlagedeaths = json.load(open('auswertungsvorlage.json'))
+cnt = 0
+for i in data:
+    year = i["Year"]
+    if int(year) in fulldates:
+        if str(i['State']) != 'United States':
+            indx = fulldates.index(int(year))
+            wertarr = vorlagedeaths[i['State']]
+            newval = i['Deaths']
+            wertarr.pop(indx)
+            wertarr.insert(indx, newval)
+            vorlagedeaths[i['State']] = wertarr
+            cnt += 1
+            print(cnt)
+
+
+with open('deathsauswertung.json', 'w') as outfile:
+    json.dump(vorlagedeaths, outfile)
+
+
+
+ufos = json.load(open(
+    '/Users/Max/Desktop/Dokumente/Theoriephase IV/Webbasierte Datenbanken/Java-Workspace/Json-Converter/UFO-Sightings_fullStatename2.json'))
+
+l = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
+     2008, 2009, 2010, 2011, 2012, 2013, 2014]
+x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+auswertung = json.load(open(
+    'auswertungsvorlage.json'))
+
+lul = 0
+wrongcounter = 0
+for j in ufos:
+    # print(j['fullState'])
+    state = str(j['fullState'])
+    if (state == "" or state == "None"):
+        print("wrong state" + str(wrongcounter))
+    else:
+        print(state)
+        temparray = auswertung[state]
+        # print(j['datetime'])
+        struct_time = time.strptime(str(j['datetime']), '%m/%d/%Y %H:%M')
+        year = time.strftime('%Y', struct_time)
+        if int(year) in l:
+            print(year)
+            idx = l.index(int(year))
+            newvalue = temparray[idx] + 1
+            temparray.pop(idx)
+            temparray.insert(idx, newvalue)
+            auswertung[state] = temparray
+            lul = lul + 1
+            print("DATA: " + str(lul))
+
+
+with open('ufoauswertung.json', 'w') as outfile:
+    json.dump(auswertung, outfile)
+
+statelist = json.load(open('stateabbrev Kopie.json'))
+enddatei = []
+for g in statelist:
+    name = str(g)
+    deathsarr = vorlagedeaths[name]
+    ufoarr = auswertung[name]
+    print('Staat: ' + name)
+    cor = numpy.corrcoef(deathsarr, ufoarr)
+    enddatei.append(name +": " + str(cor[0][1])) 
+
+with open('correlation_Drug_Poisining_mortality-UFO-Sightings.txt', 'w') as outfile:
+    for i in enddatei:
+        outfile.write(str(i)+"\n")
