008ba44a04e0d8e017b4cfa4db49adf8da9be329
mhuber97 <31443865+mhuber97@users.noreply.github.com>
2018-04-19 12:28:40 +0200

    Update testing.py

--- correlation/drugs/testing.py
+++ correlation/drugs/testing.py
@@ -3,6 +3,7 @@ import json
 from pprint import pprint
 import datetime
 import time
+import math
 
 fulldates = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
      2008, 2009, 2010, 2011, 2012, 2013, 2014]
@@ -23,14 +24,10 @@ for i in data:
             cnt += 1
             print(cnt)
 
-
 with open('deathsauswertung.json', 'w') as outfile:
     json.dump(vorlagedeaths, outfile)
 
-
-
-ufos = json.load(open(
-    '/Users/Max/Desktop/Dokumente/Theoriephase IV/Webbasierte Datenbanken/Java-Workspace/Json-Converter/UFO-Sightings_fullStatename2.json'))
+ufos = json.load(open('/Users/Max/Desktop/Dokumente/Theoriephase IV/Webbasierte Datenbanken/Java-Workspace/Json-Converter/UFO-Sightings_fullStatenames.json'))
 
 l = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
      2008, 2009, 2010, 2011, 2012, 2013, 2014]
@@ -44,17 +41,17 @@ lul = 0
 wrongcounter = 0
 for j in ufos:
     # print(j['fullState'])
-    state = str(j['fullState'])
-    if (state == "" or state == "None"):
+    state = str(j["fullState"])
+    if (state == "" or state == "None" or j["country"] != 'us'):
         print("wrong state" + str(wrongcounter))
     else:
-        print(state)
+        #print(state)
         temparray = auswertung[state]
         # print(j['datetime'])
-        struct_time = time.strptime(str(j['datetime']), '%m/%d/%Y %H:%M')
+        struct_time = time.strptime(str(j["datetime"]), '%m/%d/%Y %H:%M')
         year = time.strftime('%Y', struct_time)
         if int(year) in l:
-            print(year)
+            #print(year)
             idx = l.index(int(year))
             newvalue = temparray[idx] + 1
             temparray.pop(idx)
@@ -78,7 +75,9 @@ for g in statelist:
     ufoarr = auswertung[name]
     print('Staat: ' + name)
     cor = numpy.corrcoef(deathsarr, ufoarr)
-    enddatei.append(name +": " + str(cor[0][1])) 
+    if math.isnan(cor[0][1]):
+        cor[0][1] = 0
+    enddatei.append("\"" + name + "\": " + str(cor[0][1])) 
 
 
 
@@ -94,8 +93,19 @@ for l in allstates:
 
 usacoefficient = numpy.corrcoef(usaindexdeaths, usaindexufos)[0][1]
 
-enddatei.append('USA: ' + str(usacoefficient))
+enddatei.append('\"USA\": ' + str(usacoefficient))
 
 with open('correlation_Drug_Poisining_mortality-UFO-Sightings.txt', 'w') as outfile:
     for i in enddatei:
         outfile.write(str(i)+"\n")
+
+
+with open('correlation_Drug_Poisining_mortality-UFO-Sightings.json', 'w') as outfile:
+    outfile.write("{ \n")
+    for i in enddatei:
+        if i != len(enddatei):
+            outfile.write(str(i)+",\n")
+        else:
+            outfile.write(str(i)+"\n")
+    outfile.write("}")
+
