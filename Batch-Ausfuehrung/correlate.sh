5514f89b08c3899312d6fc241012f2e34ed5e09f
Maximilian Huber <Max@Maximilians-MacBook-Pro.local>
2018-05-09 16:37:46 +0200

    updated filenames

--- Batch-Ausfuehrung/correlate.sh
+++ Batch-Ausfuehrung/correlate.sh
@@ -2,11 +2,11 @@
 
 python3 educationcorrelation.py
 
-python3 state.py
-java -jar test.jar
-python3 testing.py
+python3 ufo_location_correction.py
+java -jar Data_Formatter.jar
+python3 deathmortalitycorrelation.py
 printf "Es folgt die Erstellung der NDJSON-Datei\n"
-java -jar tondjson.jar
+java -jar ToNdJson.jar
 
 printf "_________________________________________________________________________\n"
 printf "Sie finden die ausgewerteten Korrelationsdateien Dateien im Ordner /docs \n"
