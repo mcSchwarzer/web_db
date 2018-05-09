cd6e856a1238b1219105fb2fd4067d26f2e955cc
Maximilian Huber <Max@Maximilians-MacBook-Pro.local>
2018-05-09 18:36:35 +0200

    python2 script

---
+++ Batch-Ausfuehrung/correlate python.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python edu_correlation.py
+python ufo_location_correction.py
+java -jar Data_Formatter.jar
+python deathmortalitycorrelation.py
+python correlation_ufo_and_undp500_data.py
+
+printf "Es folgt die Erstellung der NDJSON-Datei\n"
+java -jar ToNdJson.jar
+
+printf "_________________________________________________________________________\n"
+printf "Sie finden die ausgewerteten Korrelationsdateien Dateien im Ordner /docs \n"
