451d37c326082ac66c6287a6ae6a3053b2d65d56
Maximilian Huber <Max@Maximilians-MacBook-Pro.local>
2018-05-09 10:27:18 +0200

    batchdatei

---
+++ Batch-Ausf-hrung/educationcorrelation.py
@@ -0,0 +1,62 @@
+import numpy as np
+from numpy import corrcoef as cc
+from numpy import genfromtxt
+import csv
+
+print("----import successful----")
+
+sightlist = []
+satlist = []
+actlist = []
+
+
+with open('src/sichtungen_bildung.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row['Ufo-Sichtungen pro 10.000 Einwohner'])
+        sightlist.append(float(row['Ufo-Sichtungen pro 10.000 Einwohner']))
+ 
+
+with open('src/sichtungen_bildung.csv') as csvfile2:
+    reader2 = csv.DictReader(csvfile2)
+    for row in reader2:
+        print(row['Durchschnittlicher SAT-Score'])
+        satlist.append(int(row['Durchschnittlicher SAT-Score']))
+
+with open('src/sichtungen_bildung.csv') as csvfile3:
+    reader3 = csv.DictReader(csvfile3)
+    for row in reader3:
+        print(row['Durchschnittlicher ACT-Score'])
+        actlist.append(int(row['Durchschnittlicher ACT-Score']))
+
+print(sightlist)
+print(satlist)
+print(actlist)
+print(sightlist.__sizeof__())
+print(satlist.__sizeof__())
+print(actlist.__sizeof__())
+
+print("Koeffizient SAT zu Ufo-Sichtungen mit allen Werten: ")
+print(np.corrcoef(satlist,sightlist))
+print("Koeffizient ACT zu Ufo-Sichtungen mit allen Werten: ")
+print(np.corrcoef(actlist,sightlist))
+
+#alaska entfernen
+sightlist.remove(0.0)   
+satlist.remove(757)
+actlist.remove(16)
+
+print("Koeffizient SAT zu Ufo-Sichtungen ohne Alaska (aufgrund fehlender Sichtungen): ")
+print(np.corrcoef(satlist,sightlist))
+print("Koeffizient ACT zu Ufo-Sichtungen ohne Alaska (aufgrund fehlender Sichtungen): ")
+print(np.corrcoef(actlist,sightlist))
+
+#hawaii entfernen
+sightlist.remove(0.007841944)
+satlist.remove(991)
+actlist.remove(21)
+
+print("Koeffizient SAT zu Ufo-Sichtungen ohne Alaska und Hawaii (aufgrund fehlender, sehr geringer Anzahl an Sichtungen): ")
+print(np.corrcoef(satlist,sightlist))
+print("Koeffizient ACT zu Ufo-Sichtungen ohne Alaska und Hawaii (aufgrund fehlender, sehr geringer Anzahl an Sichtungen): ")
+print(np.corrcoef(actlist,sightlist))
---
+++ Batch-Ausf-hrung/testing.py
@@ -0,0 +1,135 @@
+import numpy as numpy
+import json
+from pprint import pprint
+import datetime
+import time
+import math
+import os
+
+os.environ["GOOGLE_API_KEY"] = "AIzaSyDtEkeX2WV6xnGhvcPnZYhUO98acPjTIWo"
+
+fulldates = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
+     2008, 2009, 2010, 2011, 2012, 2013, 2014]
+
+data = json.load(open('src/drug_poisining_mortality_usa.json'))
+vorlagedeaths = json.load(open('src/auswertungsvorlage.json'))
+cnt = 0
+for i in data:
+    year = i["Year"]
+    if int(year) in fulldates:
+        if str(i['State']) != 'United States':
+            indx = fulldates.index(int(year))
+            wertarr = vorlagedeaths[i['State']]
+            newval = i['Deaths']
+            wertarr.pop(indx)
+            wertarr.insert(indx, newval)
+            vorlagedeaths[i['State']] = wertarr
+            cnt += 1
+            print(cnt)
+
+with open('src/deathsauswertung.json', 'w') as outfile:
+    json.dump(vorlagedeaths, outfile)
+
+#ufos = json.load(open('/Users/Max/Desktop/Dokumente/Theoriephase IV/Webbasierte Datenbanken/Java-Workspace/Json-Converter/UFO-Sightings_fullStatenames.json'))
+ufos = json.load(open('src/fulldata.json'))
+
+
+l = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
+     2008, 2009, 2010, 2011, 2012, 2013, 2014]
+x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+auswertung = json.load(open(
+    'src/auswertungsvorlage.json'))
+
+lul = 0
+wrongcounter = 0
+for j in ufos:
+    # print(j['fullState'])
+    state = str(j["fullState"])
+    
+    if (state == "" or state == "None" or j["country"] != 'us'):
+        print("wrong state" + str(wrongcounter))
+    else:
+        if "Washington" in state:
+            state = "Washington"
+        temparray = auswertung[state]
+
+        # print(j['datetime'])
+        struct_time = time.strptime(str(j["datetime"]), '%m/%d/%Y %H:%M')
+        year = time.strftime('%Y', struct_time)
+        if int(year) in l:
+            #print(year)
+            idx = l.index(int(year))
+            newvalue = temparray[idx] + 1
+            temparray.pop(idx)
+            temparray.insert(idx, newvalue)
+            auswertung[state] = temparray
+            lul = lul + 1
+            print("DATA: " + str(lul))
+
+
+with open('src/ufoauswertung.json', 'w') as outfile:
+    json.dump(auswertung, outfile)
+
+auswertung = json.load(open('src/ufoauswertung.json'))
+vorlagedeaths = json.load(open('src/deathsauswertung.json'))
+
+statelist = json.load(open('src/stateabbrev Kopie.json'))
+enddatei = []
+states = []
+rates = []
+for g in statelist:
+    name = str(g)
+    deathsarr = vorlagedeaths[name]
+    ufoarr = auswertung[name]
+    print('Staat: ' + name)
+    cor = numpy.corrcoef(deathsarr, ufoarr)
+    if math.isnan(cor[0][1]):
+        cor[0][1] = 0
+    states.append(name)     
+    rates.append(cor[0][1])
+    enddatei.append("\"" + name + "\": " + str(cor[0][1])) 
+
+
+
+allstates = json.load(open('src/stateabbrev Kopie.json'))
+usaindexdeaths = [0] * 16
+usaindexufos = [0] * 16
+for l in allstates:
+    deathscnt = vorlagedeaths[l]
+    ufoscnt = auswertung[l]
+    for k in range(16):
+        usaindexdeaths[k] = usaindexdeaths[k] + deathscnt[k]
+        usaindexufos[k] = usaindexufos[k] + ufoscnt[k]
+
+usacoefficient = numpy.corrcoef(usaindexdeaths, usaindexufos)[0][1]
+
+enddatei.append('\"USA\": ' + str(usacoefficient))
+states.append("USA")
+rates.append(usacoefficient)
+
+import geocoder
+
+coordinates = []
+for s in states:
+    g = geocoder.google(s)
+    lul = g.latlng
+    lng = lul[1]
+    lat = lul[0]
+    lul.clear()
+    lul.append(lng)
+    lul.append(lat)
+    print(lul)
+    coordinates.append(lul)
+
+jsonfinal = [{"State": s, "Correlation-Coefficient": r, "location": l} for s, r, l in zip(states, rates, coordinates)]
+
+with open('docs/correlation_Drug_Poisining_mortality-UFO-Sightings.txt', 'w') as outfile:
+    for i in enddatei:
+        outfile.write(str(i)+"\n")
+
+
+with open('docs/correlation_Drug_Poisining_mortality-UFO-Sightings.json', 'w') as outfile:
+    outfile.write(json.dumps(jsonfinal))
+
